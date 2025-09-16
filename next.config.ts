ad88d033b523ae2fbc34b18dbb716830c8ff5737
Pawan Kumar <pawanomen@gmail.com>
2025-09-16 23:25:29 +0000

    it is showing weather data not availbale

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -30,6 +31,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    OPENWEATHER_API_KEY: process.env.OPENWEATHER_API_KEY,
+  }
 };
 
 export default nextConfig;
